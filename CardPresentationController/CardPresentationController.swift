9a1901fd71c9a70c90c69158cbb209c3efa9c5bf
Aleksandar Vacic <av@codeaplus.com>
2018-09-15 17:08:28 +0200

    Everything starts with custom subclass of UIPresentationController

---
+++ CardPresentationController/CardPresentationController.swift
@@ -0,0 +1,13 @@
+//
+//  CardPresentationController.swift
+//  CardPresentationController
+//
+//  Copyright © 2018 Aleksandar Vacić, Radiant Tap
+//  MIT License · http://choosealicense.com/licenses/mit/
+//
+
+import UIKit
+
+class CardPresentationController: UIPresentationController {
+
+}
