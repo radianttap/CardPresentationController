e8d19450b01ac1b5e3f69abb25b1243cf40bb1d4
Aleksandar Vacic <av@codeaplus.com>
2018-10-28 15:35:18 +0100

    Added comments

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -12,15 +12,43 @@ final class ViewController: UIViewController {
 	@IBOutlet private weak var defaultPopupButton: UIButton!
 	@IBOutlet private weak var cardPopupButton: UIButton!
 
+	private lazy var transitionManager: CardTransitionManager = CardTransitionManager()
 }
 
 private extension ViewController {
+	/// Display regular, familiar full screen popup
+	///
+	/// - Parameter sender: button which initiated this action
 	@IBAction func popupDefault(_ sender: UIButton) {
 		let vc = PlainPopupController.instantiate()
+
+		//	wrap inside NC
+		let nc = UINavigationController(rootViewController: vc)
+		//	and add Done button to dismiss the popup
+		let bbi = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(ViewController.dismissPopup))
+		vc.navigationItem.leftBarButtonItem = bbi
+
+		present(nc, animated: true, completion: nil)
+	}
+
+	/// Display popup as inset card
+	///
+	/// - Parameter sender: button which initiated this action
+	@IBAction func popupCard(_ sender: UIButton) {
+		let vc = PlainPopupController.instantiate()
+
+		//	make it custom
+		vc.modalPresentationStyle = .custom
+		//	so we can use our Card transition
+		vc.transitioningDelegate = transitionManager
+
 		present(vc, animated: true, completion: nil)
 	}
 
-	@IBAction func popupCard(_ sender: UIButton) {
-
+	/// Dismisses whatever popup is currently shown
+	///
+	/// - Parameter sender: An UI object that initiated dismissal
+	@IBAction func dismissPopup(_ sender: Any) {
+		dismiss(animated: true)
 	}
 }
