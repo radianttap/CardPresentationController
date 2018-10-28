03dcda8a5a8221c17db93eb05f8a2db4ac74b5f0
Aleksandar Vacic <av@codeaplus.com>
2018-10-28 16:38:46 +0100

    Helpful methods to extract actual content controllers that initiated the transition, digging deep into UINC or UITBC

--- CardPresentationController/CardAnimator.swift
+++ CardPresentationController/CardAnimator.swift
@@ -15,6 +15,35 @@ final class CardAnimator: NSObject, UIViewControllerAnimatedTransitioning {
 	}
 
 	func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+	}
+}
 
+
+
+
+
+private extension UIViewControllerContextTransitioning {
+	var fromContentController: UIViewController? {
+		guard let topVC = viewController(forKey: .from) else { return nil }
+		return recognize(topVC)
+	}
+
+	var toContentController: UIViewController? {
+		guard let topVC = viewController(forKey: .to) else { return nil }
+		return recognize(topVC)
+	}
+
+	func recognize(_ vc: UIViewController) -> UIViewController? {
+		switch vc {
+		case let nc as UINavigationController:
+			return nc.topViewController
+
+		case let tbc as UITabBarController:
+			guard let vc = tbc.selectedViewController else { return tbc }
+			return recognize(vc)
+
+		default:
+			return vc
+		}
 	}
 }
