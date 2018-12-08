59314fb4a86e328c67e07569384816c2fe27862c
Aleksandar Vacic <av@codeaplus.com>
2018-12-09 00:06:24 +0100

    Initial version of the transition

--- CardPresentationController/CardAnimator.swift
+++ CardPresentationController/CardAnimator.swift
@@ -13,8 +13,30 @@ final class CardAnimator: NSObject, UIViewControllerAnimatedTransitioning {
 		case presentation
 		case dismissal
 	}
+
+	//	
+
 	var direction: Direction = .presentation
 
+	lazy var initialTransitionFrame: CGRect = {
+		var f: CGRect = .zero
+		f.size.width = UIScreen.main.bounds.width
+		f.origin.y = UIScreen.main.bounds.height
+		return f
+	}()
+
+	//	Configuration
+
+	var spacing: CGFloat = 34
+	var topCornerRadius: CGFloat = 20
+	var fadeAlpha: CGFloat = 0.8
+
+	//	Local stuff
+
+	private var statusBarStyle: UIStatusBarStyle = UIApplication.shared.statusBarStyle
+	private var statusBarFrame: CGRect = UIApplication.shared.statusBarFrame
+
+	//	MARK:- UIViewControllerAnimatedTransitioning
 
 	func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
 		return 0.3
@@ -23,25 +45,67 @@ final class CardAnimator: NSObject, UIViewControllerAnimatedTransitioning {
 	func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
 		guard
 			let fromVC = transitionContext.viewController(forKey: .from),
-			let toVC = transitionContext.viewController(forKey: .to)
+			let toVC = transitionContext.viewController(forKey: .to),
+			let fromView = fromVC.view,
+			let toView = toVC.view
 		else {
 			return
 		}
 		let containerView = transitionContext.containerView
 		let duration = transitionDuration(using: transitionContext)
+		let ratio: CGFloat = 0.86
 
 		switch direction {
 		case .presentation:
-			containerView.addSubview(toVC.view)
-			break
+			let fromBeginFrame = transitionContext.initialFrame(for: fromVC)
+			let fromEndFrame = fromBeginFrame.inset(by: UIEdgeInsets(top: statusBarFrame.height, left: 0, bottom: 0, right: 0))
+
+			let toStartFrame = initialTransitionFrame
+			let toBaseFinalFrame = transitionContext.finalFrame(for: toVC)
+			let toEndFrame = toBaseFinalFrame.inset(by: UIEdgeInsets(top: statusBarFrame.height + spacing, left: 0, bottom: 0, right: 0))
+
+			toView.clipsToBounds = true
+			toView.frame = toStartFrame
+			containerView.addSubview(toView)
+
+			let pa = UIViewPropertyAnimator(duration: duration, dampingRatio: ratio) {
+				[weak self] in
+				guard let self = self else { return }
+
+				fromView.frame = fromEndFrame
+				toView.frame = toEndFrame
+				fromView.maskTopCard(cornerRadius: self.topCornerRadius)
+				toView.maskTopCard(cornerRadius: self.topCornerRadius)
+				fromView.alpha = self.fadeAlpha
+			}
+			pa.addCompletion {
+				[weak self] _ in
+				transitionContext.completeTransition(true)
+				self?.direction = .dismissal
+			}
+			pa.startAnimation()
 
 		case .dismissal:
-			fromVC.view.removeFromSuperview()
+			let fromEndFrame = initialTransitionFrame
+			let toEndFrame = transitionContext.finalFrame(for: toVC)
+
+			let pa = UIViewPropertyAnimator(duration: duration, dampingRatio: ratio) {
+				fromView.maskTopCard(cornerRadius: 0)
+				toView.maskTopCard(cornerRadius: 0)
+				fromView.frame = fromEndFrame
+				toView.frame = toEndFrame
+				toView.alpha = 1
+				fromView.alpha = 1
+			}
+			pa.addCompletion {
+				[weak self] _ in
+				transitionContext.completeTransition(true)
+				fromView.removeFromSuperview()
+				self?.direction = .presentation
+			}
+			pa.startAnimation()
 			break
 		}
-
-
-		transitionContext.completeTransition(true)
 	}
 }
 
