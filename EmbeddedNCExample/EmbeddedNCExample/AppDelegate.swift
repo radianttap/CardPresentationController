875f5a7cf5e07d644f992e2d1c027a582746d660
Aleksandar Vacic <av@codeaplus.com>
2018-12-14 16:10:50 +0100

    Added another example
    This one is using UINC embedded in another ordinary UIVC.
    Dismissal has an interesting (unwanted) side effect in this scenario)

---
+++ EmbeddedNCExample/EmbeddedNCExample/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  EmbeddedNCExample
+//
+//  Created by Aleksandar Vacić on 12/14/18.
+//  Copyright © 2018 Radiant Tap. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+	var window: UIWindow?
+
+
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		return true
+	}
+}
+
---
+++ EmbeddedNCExample/EmbeddedNCExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EmbeddedNCExample
+//
+//  Created by Aleksandar Vacić on 12/14/18.
+//  Copyright © 2018 Radiant Tap. All rights reserved.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+	private var controller: UIViewController?
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		let vc = ContentController.instantiate()
+		let nc = UINavigationController(rootViewController: vc)
+		nc.navigationBar.barTintColor = .red
+		embed(controller: nc, into: view)
+		controller = nc
+	}
+}
+
