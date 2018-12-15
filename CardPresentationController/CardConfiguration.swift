a2bc5315af9ef7c9f3e3b45a56d28d7b061d33e1
Aleksandar Vacic <av@codeaplus.com>
2018-12-15 21:16:58 +0100

    Added lots of documentation. Also...
    added option to specify the kind of blur you want to apply to the far-back card, in order to prevent content overlap and thus visual noise

--- CardPresentationController/CardConfiguration.swift
+++ CardPresentationController/CardConfiguration.swift
@@ -12,23 +12,41 @@ import UIKit
 ///
 ///	Supply it as optional parameter to `presentCard(...)`.
 public struct CardConfiguration {
+	///	Vertical inset from the top or already shown card
 	var verticalSpacing: CGFloat = 16
+
+	///	Leading and trailing inset for the existing (presenting) view when it's being moved to the back
 	var horizontalInset: CGFloat = 16
+
+	///	Card has rounded corners, right?
 	var cornerRadius: CGFloat = 12
+
+	///	The starting frame position for the card
+	var initialTransitionFrame: CGRect?
+
+	///	How much to fade the back card.
+	///
+	///	Ignored if back card is UINavigationController
 	var backFadeAlpha: CGFloat = 0.8
 
-	var initialTransitionFrame: CGRect?
+	///	If `true`, a temporary `UIVisualEffectView` with given UIBlurEffect.Style will be installed behind the already existing back card.
+	///	This helps prevent noisy content overlap from multiple stacked cards.
+	///
+	///	Ignored if back card to which it should corresponds to, is actually `UINavigationController`.
+	///	Also if `backFadeAlpha is `1.0`.
+	var blurredBackViewStyle: UIBlurEffect.Style?
 
 	///	Default initializer, with most suitable values
 	init() {}
 }
 
 extension CardConfiguration {
-	///	Very convinient initializer; supply only those params which are different from default ones.
+	///	Very convenient initializer; supply only those params which are different from default ones.
 	public init(verticalSpacing: CGFloat? = nil,
 		 horizontalInset: CGFloat? = nil,
 		 cornerRadius: CGFloat? = nil,
 		 backFadeAlpha: CGFloat? = nil,
+		 blurredBackViewStyle: UIBlurEffect.Style? = nil,
 		 initialTransitionFrame: CGRect? = nil)
 	{
 		if let verticalSpacing = verticalSpacing {
@@ -45,6 +63,10 @@ extension CardConfiguration {
 
 		if let backFadeAlpha = backFadeAlpha {
 			self.backFadeAlpha = backFadeAlpha
+		}
+
+		if let blurredBackViewStyle = blurredBackViewStyle {
+			self.blurredBackViewStyle = blurredBackViewStyle
 		}
 
 		if let initialTransitionFrame = initialTransitionFrame {
